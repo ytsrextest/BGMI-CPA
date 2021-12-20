e948bb4465a4053d62b9eebcf76891567118806c
ytsrextest <68838379+ytsrextest@users.noreply.github.com>
2021-12-20 09:23:34 +0530

    Update jsform.js

--- jsform.js
+++ jsform.js
@@ -1,4 +1,4 @@
-$(document).ready(function () {
+$("document").ready(function () {
  // alert('hiiiii');
     $("#submit").click(function () {
         $(".alert-success").slideToggle("slow").delay(2000).slideToggle("slow");
