a5afab401a05af29b9ce471e7571b9db04e16363
ytsrextest <68838379+ytsrextest@users.noreply.github.com>
2021-12-20 08:56:05 +0530

    Create jsform.css

---
+++ jsform.css
@@ -0,0 +1,6 @@
+$(document).ready(function () {
+ // alert('hiiiii');
+    $("#submit").click(function () {
+        $(".alert-success").slideToggle("slow").delay(2000).slideToggle("slow");
+    });
+});
